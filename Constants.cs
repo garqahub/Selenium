6ee56b572dea387a118a4ce18a57ed0d532e8a2a
garqahub <michael.schultz@garmin.com>
2015-08-26 14:04:18 -0500

    added code

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace SauceLabs.SeleniumExamples
+{
+    /// <summary>contains constants used by the tests such as the user name and password for the sauce labs</summary>
+    internal static class Constants
+    {
+        /// <summary>name of the sauce labs account that will be used</summary>
+        internal const string SAUCE_LABS_ACCOUNT_NAME = "onthesauce";
+        /// <summary>account key for the sauce labs account that will be used</summary>
+        internal const string SAUCE_LABS_ACCOUNT_KEY = "c5884aea-ac3f-4fcd-9272-125cdec4cb48";
+
+        // NOTE:
+        // To change the maximum number of parallel tests edit DegreeOfParallelism in AssemblyInfo.cs
+
+    }
+}
